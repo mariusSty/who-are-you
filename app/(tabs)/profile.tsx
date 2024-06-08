a6a04dc8046d376946f13f54e67276c022d491f4
Marius STEPHANY <marius.stephany@gmail.com>
2024-06-08 16:02:14 +0200

    :sparkles: Get results on Profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,8 +1,27 @@
 import { Text, View } from "@/components/Themed";
 import quizDatas from "@/constants/data.json";
+import * as SecureStore from "expo-secure-store";
+import { useCallback, useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 
 export default function Profile() {
+  const [quizResults, setQuizResults] = useState<
+    Array<{ slug: string; result: string | null }>
+  >([]);
+
+  const getResults = useCallback(async () => {
+    const results: Array<{ slug: string; result: string | null }> = [];
+    for (const quiz of quizDatas) {
+      const quizResult = await SecureStore.getItemAsync(quiz.slug);
+      results.push({ slug: quiz.slug, result: quizResult });
+    }
+    setQuizResults(results);
+  }, []);
+
+  useEffect(() => {
+    getResults();
+  }, [getResults]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Profil</Text>
@@ -15,7 +34,10 @@ export default function Profile() {
         {quizDatas.map((quizData) => (
           <View key={quizData.slug} style={styles.quizResultItem}>
             <Text style={styles.quizResultItemTitleText}>{quizData.title}</Text>
-            <Text style={styles.quizResultItemResultText}>Result</Text>
+            <Text style={styles.quizResultItemResultText}>
+              {quizResults.find((result) => result.slug === quizData.slug)
+                ?.result || "Test non effectué"}
+            </Text>
           </View>
         ))}
       </View>
