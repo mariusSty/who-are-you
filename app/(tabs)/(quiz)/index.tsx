e68fde17fe32544e3e7fd070c22fee43963e0218
Marius STEPHANY <marius.stephany@gmail.com>
2024-06-08 12:48:44 +0200

    :sparkles: Add quiz datas title on profile and main pages

--- app/(tabs)/(quiz)/index.tsx
+++ app/(tabs)/(quiz)/index.tsx
@@ -1,8 +1,8 @@
+import { Text, View } from "@/components/Themed";
+import quizDatas from "@/constants/data.json";
 import { StyleSheet } from "react-native";
 
-import { Text, View } from "@/components/Themed";
-
-export default function TabOneScreen() {
+export default function Quiz() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Quiz</Text>
@@ -11,6 +11,13 @@ export default function TabOneScreen() {
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
+      <View style={styles.quizContainer}>
+        {quizDatas.map((quizData) => (
+          <View key={quizData.slug} style={styles.quizButton}>
+            <Text style={styles.quizButtonText}>{quizData.title}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
@@ -20,6 +27,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    gap: 20,
   },
   title: {
     fontSize: 20,
@@ -29,5 +37,23 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: "80%",
+  },
+  quizContainer: {
+    gap: 15,
+    width: "80%",
+    alignItems: "stretch",
+    justifyContent: "center",
+  },
+  quizButton: {
+    flexDirection: "row",
+    borderWidth: 2,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  quizButtonText: {
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,8 +1,8 @@
+import { Text, View } from "@/components/Themed";
+import quizDatas from "@/constants/data.json";
 import { StyleSheet } from "react-native";
 
-import { Text, View } from "@/components/Themed";
-
-export default function TabTwoScreen() {
+export default function Profile() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Profil</Text>
@@ -11,6 +11,14 @@ export default function TabTwoScreen() {
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
+      <View style={styles.quizResultContainer}>
+        {quizDatas.map((quizData) => (
+          <View key={quizData.slug} style={styles.quizResultItem}>
+            <Text style={styles.quizResultItemTitleText}>{quizData.title}</Text>
+            <Text style={styles.quizResultItemResultText}>Result</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
@@ -20,14 +28,35 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    gap: 20,
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
   },
   separator: {
-    marginVertical: 30,
     height: 1,
     width: "80%",
+  },
+  quizResultContainer: {
+    gap: 15,
+    width: "80%",
+    alignItems: "stretch",
+    justifyContent: "center",
+  },
+  quizResultItem: {
+    flexDirection: "row",
+    borderWidth: 2,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+  quizResultItemTitleText: {
+    fontSize: 14,
+  },
+  quizResultItemResultText: {
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
